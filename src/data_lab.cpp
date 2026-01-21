d3d140791e40578c394a2047538ddc95fe418c1b
Avery <a2432060765@gmail.com>
2026-01-21 21:15:30 +0800

    subtract added

--- src/data_lab.cpp
+++ src/data_lab.cpp
@@ -4,11 +4,117 @@
 namespace data_lab {
 
 int32_t add(int32_t a, int32_t b) {
-    return a + b;
+    uint32_t ua = static_cast<uint32_t>(a);
+    uint32_t ub = static_cast<uint32_t>(b);
+
+    uint32_t sum = ua ^ ub; 
+    uint32_t carry = ua & ub; carry <<= 1; 
+
+    uint32_t sumc = sum; 
+    uint32_t carryc = carry; //Use copied variables to store the temporay value. 
+    //First repetation done. 
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    sum = sumc ^ carryc; 
+    carry = sumc & carryc; carry <<= 1; 
+    sumc = sum; carryc = carry; //1time
+    
+
+    //repeat 32 times because the max bit is 32. 
+    
+    return static_cast<int32_t>(sum);
 }
 
 int32_t subtract(int32_t a, int32_t b) {
-    return a - b;
+    return a;
 }
 
 int32_t multiply(int32_t a, int32_t b) {
